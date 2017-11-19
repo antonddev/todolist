4f76f5a78ec655b999979aa3218da4c05a8c317c
Anton <anton.ddev@gmail.com>
2017-11-19 22:56:45 +0200

    Added initial AJAX call ($.getJSON)

--- public/app.js
+++ public/app.js
@@ -1,1 +1,15 @@
-alert("Hi from app.js");
+$(document).ready(function(){  // .ready - as long as DOM is ready, the function will run
+  $.getJSON('/api/todos')
+  .then(addTodos)
+  .catch(function(err){
+    console.log(err);
+  })
+});
+
+// add todos to page
+function addTodos(todos){
+  todos.forEach(function(todo){
+    var newTodo = $('<li class="task">' + todo.name + '</li>');
+    $('.list').append(newTodo);
+  });
+}
