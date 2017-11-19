6d61e0d5af1b56dc19dd9c88fdfba2035797cdf6
Anton <anton.ddev@gmail.com>
2017-11-20 01:38:42 +0200

    Update functionality is added

--- public/app.js
+++ public/app.js
@@ -12,7 +12,12 @@ $(document).ready(function(){  // .ready - as long as DOM is ready, the function
     }
   })
 
-  $('.list').on('click', 'span', function(){ // adding a click listener to ul(.list) itself, because it was on the page from the begining. Passing 'span' (dynamically added element) as the second argument to set listener on it.
+  $('.list').on('click', 'li', function(){
+    updateTodo($(this));
+  })
+
+  $('.list').on('click', 'span', function(event){ // adding a click listener to ul(.list) itself, because it was on the page from the begining. Passing 'span' (dynamically added element) as the second argument to set listener on it.
+    event.stopPropagation(); // this will stop the event from bubbling up (so when we clicked on a span, it's not also going to triger the parent <li>)
     removeTodo($(this).parent());
   })
 });
@@ -27,7 +32,8 @@ function addTodos(todos){
 // add single todo to page
 function addTodo(todo){
   var newTodo = $('<li class="task">' + todo.name + ' <span>X</span></li>');
-  newTodo.data('id', todo._id); // adding todo id to jQuery data to ease the DELETE functionality maintaining
+  newTodo.data('id', todo._id); // adding todo id to jQuery memory (data) to ease the DELETE functionality maintaining
+  newTodo.data('completed', todo.completed); // for easy 'PUT'(updateTodo) maintaining
   if(todo.completed){
     newTodo.addClass("done");
   }
@@ -44,6 +50,21 @@ function createTodo(){
   })
   .catch(function(err){
     console.log(err);
+  })
+}
+
+function updateTodo(todo){
+  var updateUrl = '/api/todos/' + todo.data('id');
+  var isDone = !todo.data('completed'); // grab true or false from here // isDone is what we want the value to be (opposite from the qurrent value)
+  var updateData = {completed: isDone};
+  $.ajax({
+    method: 'PUT',
+    url: updateUrl,
+    data: updateData
+  })
+  .then(function(updatedTodo){
+    todo.toggleClass('done');
+    todo.data('completed', isDone);
   })
 }
 
